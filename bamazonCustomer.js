d883bec6b6076945d89c25b1d0c35dab820a5a1a
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-09-03 19:13:09 -0400

    added connection variable for locally hosted mysql server

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,22 @@
 var inquirer = require("inquirer")
 var sql = require("sql")
 
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 8889,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "root",
+    database: "BAMAZON"
+  });
+  
+
+  
 
 
 console.log("Welcome to Bamazon!\n\n====================")
