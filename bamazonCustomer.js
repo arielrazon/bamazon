6cbf1d175978469225b4568a1e8a23a165a0efe2
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-09-03 19:35:36 -0400

    added sql query and if statements for it customer product is found

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,18 +4,18 @@ var sql = require("sql")
 
 var connection = mysql.createConnection({
     host: "localhost",
-  
+
     // Your port; if not 3306
     port: 8889,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "root",
     database: "BAMAZON"
-  });
-  
+});
+
 
 console.log("Welcome to Bamazon!\n\n====================")
 
@@ -33,10 +33,30 @@ inquirer
         }
     ])
     .then(answers => {
-        connection.connect(function(err) {
+        connection.connect(function (err) {
             if (err) throw err;
             console.log("connected as id " + connection.threadId);
-         
-          });
-        
+            queryProductID(answers.product_id)
+        });
+
     });
+
+function queryProductID(input) {
+    connection.query("SELECT * FROM PRODUCTS WHERE ITEM_ID=" + input, function (err, res) {
+        if (err) throw err;
+        if (res.STOCK_QUANTITY > 1) {
+            console.log("Your search found:\n\nItem ID | Product | Department | Price | Stock Remaining\n")
+            console.log(res.ITEM_ID + " | " + res.PRODUCT_NAME + " | " + res.DEPARTMENT_NAME + " | $" + res.PRICE + " | " + res.STOCK_QUANTITY + " units");
+            inquirer.prompt([{
+
+            }]).then(yesNo)
+        }
+
+
+        else if (res.STOCK_QUANTITY === 0) {
+            console.log("\nI'm sorry but " + res.PRODUCT_NAME + " is out of stock!")
+        }
+
+        console.log("\n-----------------------------------");
+    });
+}
