6e1741df619d51f264ceeb50973d3aec07fbd58c
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-09-03 21:04:43 -0400

    added nested menus for if customer wants to buy again

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -56,9 +56,8 @@ once update goes through show customer total cost of purchase
 function queryProductID(input, quantity) {
     connection.query("SELECT * FROM PRODUCTS WHERE ITEM_ID=" + input, function (err, res) {
         if (err) throw err;
-        stock = res[0].STOCK_QUANTITY
-        console.log(res[0])
-        console.log(stock)
+        stock = res[0].STOCK_QUANTITY;
+        var price = res[0].PRICE;
         if (res[0].STOCK_QUANTITY > 1) {
             console.log("Your search found:\n\nItem ID | Product | Department | Price | Stock Remaining\n")
             console.log(res[0].ITEM_ID + " | " + res[0].PRODUCT_NAME + " | " + res[0].DEPARTMENT_NAME + " | $" + res[0].PRICE + " | " + res[0].STOCK_QUANTITY + " units");
@@ -68,25 +67,51 @@ function queryProductID(input, quantity) {
                 "message": "Do you wish to proceed with your order?"
             }]).then(yesNo => {
                 if (yesNo.yesNo) {
-                    connection.query("UPDATE PRODUCTS SET STOCK_QUANTITY = " + (stock - quantity) + "WHERE ITEM_ID = " + input)
+                    connection.query("UPDATE PRODUCTS SET STOCK_QUANTITY = " + (stock - quantity) + " WHERE ITEM_ID = " + input, function (err, res) {
+                        if (err) throw err;
+
+                        console.log("Your order has been placed!\n\n The total cost is: $" + (price * quantity) + "\n\nThank you for your business!\n\n");
+                        buyAgain();
+                    }
+                    );
                 }
                 else {
                     console.log("OK! Let us know if you change your mind!");
                     connection.end(function (err) {
                         if (err) throw err
                         console.log("Connection ended")
-                    })
+                    });
                 }
             })
         }
         else if (res[0].STOCK_QUANTITY === 0) {
-            console.log("\nI'm sorry but " + res.PRODUCT_NAME + " is out of stock!")
+            console.log("\nI'm sorry but " + res.PRODUCT_NAME + " is out of stock!");
             connection.end(function (err) {
                 if (err) throw err
                 console.log("Connection ended")
-            })
-        }
+            });
+        };
 
         console.log("\n-----------------------------------");
     });
-}
+};
+
+function buyAgain() {
+    inquirer.prompt([{
+        type: "confirm",
+        message: "Is there anything else you would like to buy today?",
+        name: "newPurchase"
+
+    }]), then(result => {
+        if (result.newPurchase) {
+            initialPrompt();
+        }
+        else {
+            console.log("Thank you for your business! Please come back again soon");
+            connection.end(function (err) {
+                if (err) throw err;
+                console.log("Connection ended");
+            });
+        };
+    });
+};
